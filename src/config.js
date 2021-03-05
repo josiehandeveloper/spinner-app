99c3023968b1aff5c18bb86e066c745844e5957f
Josephine Han <jsphnhan@gmail.com>
2021-03-04 16:45:07 -0800

    config

--- src/config.js
+++ src/config.js
@@ -2,13 +2,7 @@ module.exports = {
   PORT: process.env.PORT || 8080,
   API_BASE_URL: process.env.API_BASE_URL || "http://localhost:8000",
   TOKEN_KEY: "watchit-app-token",
-<<<<<<< HEAD
   API_ENDPOINT: "https://stark-ocean-20108.herokuapp.com",
   API_KEY: process.env.REACT_APP_API_KEY || "3dbc1bc5de0aea0ac4c73f5bc75d2b16",
   DATABASE_URL: "postgresql://josephinehan@localhost/watchit",
-=======
-  API_ENDPOINT: "https://serene-gorge-32330.herokuapp.com",
-  API_KEY: process.env.REACT_APP_API_KEY,
-  DB_URL: "postgresql://josephinehan@localhost/watchit",
->>>>>>> b1a19868c2b62ff43d3909da0a7834ba471c4b70
 };
