cd195d68de15a311d3d430a388f430db6188585e
Josephine Han <jsphnhan@gmail.com>
2021-03-05 15:52:59 -0800

    removed cors anywhere

--- src/config.js
+++ src/config.js
@@ -3,8 +3,7 @@ module.exports = {
   API_BASE_URL: process.env.API_BASE_URL || "http://localhost:8000",
   TOKEN_KEY: "watchit-app-token",
   API_ENDPOINT:
-    process.env.API_ENDPOINT ||
-    "https://cors-anywhere.herokuapp.com/https://murmuring-crag-13918.herokuapp.com",
+    process.env.API_ENDPOINT || "https://murmuring-crag-13918.herokuapp.com",
   API_KEY: process.env.REACT_APP_API_KEY || "3dbc1bc5de0aea0ac4c73f5bc75d2b16",
   DATABASE_URL: "postgresql://josephinehan@localhost/watchit",
 };
