29040a8919dc536b55a6da4d02da346ccbd0e173
Josephine Han <jsphnhan@gmail.com>
2021-03-16 16:09:25 -0700

    movies &&

--- src/Components/MyList/MyList.js
+++ src/Components/MyList/MyList.js
@@ -12,22 +12,27 @@ export default class MyList extends React.Component {
       <div className="list">
         <h1>My List</h1>
         <div className="list-container">
-          {movies.map((movie) => (
-            <div className="movie" key={movie.id}>
-              <img alt="poster" width="250" src={IMG_API + movie.poster_path} />
-              <div className="movie-info">
-                <h3>{movie.title}</h3>
-                <p>Rating: {movie.vote_average}</p>
+          {movies &&
+            movies.map((movie) => (
+              <div className="movie" key={movie.id}>
+                <img
+                  alt="poster"
+                  width="250"
+                  src={IMG_API + movie.poster_path}
+                />
+                <div className="movie-info">
+                  <h3>{movie.title}</h3>
+                  <p>Rating: {movie.vote_average}</p>
 
-                <button
-                  className="deletebtn"
-                  onClick={() => this.context.deleteMovie(movie.id)}
-                >
-                  Delete
-                </button>
+                  <button
+                    className="deletebtn"
+                    onClick={() => this.context.deleteMovie(movie.id)}
+                  >
+                    Delete
+                  </button>
+                </div>
               </div>
-            </div>
-          ))}
+            ))}
         </div>
       </div>
     );
