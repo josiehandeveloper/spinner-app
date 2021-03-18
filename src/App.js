55afd4beacb5a00ded77ac676b2cae6154ce00be
Josephine Han <jsphnhan@gmail.com>
2021-03-17 18:25:26 -0700

    console.log(movies) on UserMovies

--- src/App.js
+++ src/App.js
@@ -73,6 +73,7 @@ export default class App extends Component {
         .then((res) => res.json())
         .then((movies) => {
           this.setState({ movies });
+          console.log(movies);
         });
     },
     /* Homepage */
